acb72aa1885d97775326cd019666fbf44c9e78a2
Suliman <sssuliman@bk.ru>
2023-07-31 12:41:02 +0300

    Исправил поведение категорий

--- models/Categories.model.js
+++ models/Categories.model.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const categoriesSchema = mongoose.Schema({
   name: String,
   photo: String,
-  globalCategories: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "GlobalCategories"
-  }
+  // globalCategories: {
+  //   type: mongoose.SchemaTypes.ObjectId,
+  //   ref: "GlobalCategories"
+  // }
 });
 
 const Categories = mongoose.model("Categories", categoriesSchema);
