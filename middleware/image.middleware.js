a53f0542e0815957b6b2850f10aa15762949f936
Suliman <sssuliman@bk.ru>
2023-08-05 17:39:58 +0300

    fix: Исправил поведение image.middleware

--- middleware/image.middleware.js
+++ middleware/image.middleware.js
@@ -1,23 +1,43 @@
 const multer = require("multer");
 
-// создание Multer для добавления картинок
-const storage = multer.diskStorage({
-    destination:(_,__,cb) => {
-      cb(null,'images')
-    },
-    filename:(_,file,cb) => {
-      cb(null, file.originalname)
-    }
-  })
-
-  const types = ["image/png", "image/jpeg", "image/jpg", "image/svg"];
-
-  const fileFilter = ( req, file, cb) => {
-
-	if (types.includes(file.mimetype)) {
-		cb(null, true);
-	} else {
-		cb(null, false);
-	}
+// Функция для определения пути сохранения в зависимости от типа
+const getDestination = (type) => {
+  switch (type) {
+    case "products":
+      return "images/products";
+    case "users":
+      return "images/users";
+    case "topical":
+      return "images/topical";
+  }
 };
-module.exports = multer({ storage, fileFilter })
+
+// Функция для создания Multer объекта
+const createMulter = (type) => {
+  return multer({
+    storage: multer.diskStorage({
+      destination: (_, __, cb) => {
+        const destination = getDestination(type);
+        cb(null, destination);
+      },
+      filename: (_, file, cb) => {
+        cb(null, file.originalname);
+      },
+    }),
+    fileFilter: (req, file, cb) => {
+      const acceptedTypes = [
+        "image/png",
+        "image/jpeg",
+        "image/jpg",
+        "image/svg",
+      ];
+      if (acceptedTypes.includes(file.mimetype)) {
+        cb(null, true);
+      } else {
+        cb(null, false);
+      }
+    },
+  });
+};
+
+module.exports = createMulter;
