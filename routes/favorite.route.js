6c93b04399b0a4b3ae1df036741eb80feba42292
Suliman <sssuliman@bk.ru>
2023-08-06 21:19:28 +0300

    fix: Изменить post запрос на patch у favorite

--- routes/favorite.route.js
+++ routes/favorite.route.js
@@ -2,10 +2,9 @@ const Router = require("express");
 const { favoriteController } = require("../controllers/favorite.controller");
 const router = Router();
 
-
 router.get("/:id", favoriteController.getFavorite);
-router.post("/create/:id", favoriteController.createFavorite)
-router.post("/add/:id", favoriteController.addToFavorite);
+router.post("/create/:id", favoriteController.createFavorite);
+router.patch("/add/:id", favoriteController.addToFavorite);
 router.delete("/delete/:id", favoriteController.deleteInFavorite);
 
 module.exports = router;
